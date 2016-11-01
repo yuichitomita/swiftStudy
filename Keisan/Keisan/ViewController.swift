6d7d63d5b56271ccfed9e4503c152a07533552e3
yuichitomita <y.tomita@speedlink-td.info>
2016-11-01 13:01:58 +0900

    Keisan プロジェクト追加

---
+++ Keisan/Keisan/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Keisan
+//
+//  Created by Tomi on 2016/11/01.
+//  Copyright © 2016年 slj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var count = 0
+    var minus = 0
+    var kakezan = 0
+    var warizan = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        count = 10
+        count = 11
+        
+        count = count + 1
+        
+        minus = 10
+        minus = minus - 5
+        
+        kakezan = 10
+        kakezan = kakezan * 10
+        
+        warizan = 10
+        warizan = warizan / 2
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
